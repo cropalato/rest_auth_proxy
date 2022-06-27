a832ab7420d4365ff2c3c8426141ade0d95ad8f1
Ricardo Melo <rmelo@ludia.com>
2022-06-27 16:21:26 -0400

    Adding config load file

---
+++ config.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"io/ioutil"
+
+	"gopkg.in/yaml.v3"
+)
+
+type Authorization struct {
+	Method    string   `yaml:"method"`
+	PathRegEx []string `yaml:"pathregex"`
+}
+
+//type ConfigFile map[string][]Authorization
+type ConfigFile struct {
+	Listen         string                     `yaml:"listen"`
+	Pdns_api_url   string                     `yaml:"pdns_api_url"`
+	Pdns_api_token string                     `yaml:"pdns_api_token"`
+	DebugMode      bool                       `yaml:"debugMode"`
+	Rules          map[string][]Authorization `yaml:"rules"`
+}
+
+var (
+	config ConfigFile
+)
+
+func (cfg *ConfigFile) loadConfig(path string) error {
+	file, err := ioutil.ReadFile(path)
+	if err != nil {
+		return err
+	}
+
+	err = yaml.Unmarshal(file, cfg)
+	if err != nil {
+		return err
+	}
+	return nil
+}
