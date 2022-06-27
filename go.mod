f4ca630c6e805ff1c124c8ff3b2e261bd65f7c62
Ricardo Melo <rmelo@ludia.com>
2022-06-27 16:20:32 -0400

    Adding config load file

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/cropalato/pdnsapi-auth-proxy
 
 go 1.18
+
+require (
+	gopkg.in/yaml.v3 v3.0.1
+	k8s.io/klog v1.0.0
+)
--- main.go
+++ main.go
@@ -5,18 +5,19 @@ import (
 	"flag"
 	"fmt"
 	"log"
-	"log/syslog"
 	"net/http"
 	"os"
 	"strconv"
+
+	"k8s.io/klog"
 )
 
 var (
-	lg, lg_err            = syslog.New(syslog.LOG_INFO, "pdnsAPI_auth_proxy")
-	pdns_api_url   string = "http://localhost:8100"
+	cfgFile        string = "./config.yaml"
+	pdns_api_url   string = ""
 	pdns_api_token string = ""
 	listen         string = ":9090"
-	debugMode      bool   = false
+	proxyRules     map[string][]Authorization
 )
 
 func LookupEnvOrString(key string, defaultVal string) string {
@@ -38,34 +39,35 @@ func LookupEnvOrBool(key string, defaultVal bool) bool {
 }
 
 func main() {
-	if lg_err != nil {
-		log.Fatalln(lg_err)
-	}
+	klog.InitFlags(nil)
+	defer klog.Flush()
 
+	flag.StringVar(&cfgFile, "config-file", LookupEnvOrString("PAP_CONFIG_FILE", cfgFile), "Config File.")
 	flag.StringVar(&listen, "listen", LookupEnvOrString("PAP_LISTEN", listen), "Listenning socket.")
 	flag.StringVar(&pdns_api_url, "url", LookupEnvOrString("PAP_PDNS_API_URL", pdns_api_url), "PowerDNS server API URL.")
 	flag.StringVar(&pdns_api_token, "pdns-api-token", LookupEnvOrString("PAP_PDNS_API_TOKEN", pdns_api_url), "PowerDNS server API TOKEN.")
-	flag.BoolVar(&debugMode, "debug", LookupEnvOrBool("PAP_DEBUG", debugMode), "Enable debug mode.")
 	flag.Parse()
-	lg.Debug(fmt.Sprintf("Finished process info will be send to %s.", pdns_api_url))
 
-	if debugMode {
-		lg, lg_err = syslog.New(syslog.LOG_DEBUG, "pdnsAPI_auth_proxy")
+	if err := config.loadConfig(cfgFile); err != nil {
+		log.Fatalf("Error loading config file %v. %v\n", cfgFile, err)
 	}
 
-	if lg_err != nil {
-		log.Fatalln(lg_err)
+	if pdns_api_url == "" {
+		pdns_api_url = config.Pdns_api_url
 	}
+	if pdns_api_token == "" {
+		pdns_api_token = config.Pdns_api_token
+	}
+	if len(config.Rules) == 0 {
+		log.Fatalf("Missing Rules from config file.")
+	}
+	proxyRules = config.Rules
 
-	if debugMode {
-		fmt.Printf("%s=%v\n", "listen", listen)
-		fmt.Printf("%s=%v\n", "pdns_api_url", pdns_api_url)
-		fmt.Printf("%s=%v\n", "debugMode", debugMode)
-	}
+	klog.V(5).Info(fmt.Sprintf("Finished process info will be send to %s.", pdns_api_url))
 
 	http.HandleFunc("/", proxyHandler)
 
-	lg.Info(fmt.Sprintf("Listening http requests from %s.", listen))
+	klog.V(1).Info(fmt.Sprintf("Listening http requests from %s.", listen))
 	err := http.ListenAndServe(listen, nil)
 	if errors.Is(err, http.ErrServerClosed) {
 		fmt.Printf("server closed\n")
